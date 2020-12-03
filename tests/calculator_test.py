977ff50e78cf008d00e00813aed04817c9246117
Ewan Brain <ewan.brain@gmail.com>
2020-12-03 09:11:16 +0000

    imports fixed

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,7 +1,16 @@
 import unittest
-from modules import calculator
+from modules.calculator import add, subtract, multiply, divide
 
 class TestCalculator(unittest.TestCase):
 
     def test_can_add(self):
-        self.assertEqual(10, calculator.add(5,5))
+        self.assertEqual(10, add(5,5))
+
+    def test_can_subtract(self):
+        self.assertEqual(2, subtract(8,6))
+
+    def test_can_multiply(self):
+        self.assertEqual(10, multiply(5, 2))
+
+    def test_can_divide(self):
+        self.assertEqual(5, divide (10, 2))
--- tests/run_tests.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from modules import calculator
-from controllers import controller
-
-if __name__ == '__main__':
-    unittest.main()
